baef1a5b0beb036420e6683f306857be1d6a1ac6
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-09 02:09:02 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,8 @@
-def current_age_for_birth_year
-  age_of_person = current_age_for_birth_year(1990)
-  puts "current_age_for_birth_year #{age_of_person}."
+# def current_age_for_birth_year
+#   age_of_person = current_age_for_birth_year(1990)
+#   puts "current_age_for_birth_year #{age_of_person}."
+# end
+
+def current_age_for_birth_year(birth_year)
+  Time.now.year - birth_year
 end
